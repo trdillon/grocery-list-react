6f28e8e233b3bbf9c3299db81dd54e7f4b29d3a3
Tim Dillon <tim@itsdits.com>
2020-11-17 07:31:05 +0900

    Created GroceryService

---
+++ src/service/GroceryService.js
@@ -0,0 +1,49 @@
+import http from "../http-common";
+
+const getAll = () => {
+    return http.get("/grocery");
+};
+
+const getByName = name => {
+    return http.get(`/grocery?name=${name}`);
+};
+
+const get = id => {
+    return http.get(`/grocery/${id}`);
+};
+
+const getNeeded = () => {
+    return http.get("/grocery/need");
+}
+
+const getPurchased = () => {
+    return http.get("/grocery/purchased");
+}
+
+const add = data => {
+    return http.post("/grocery", data);
+};
+
+const edit = (id, data) => {
+    return http.put(`/grocery/${id}`, data);
+};
+
+const remove = id => {
+    return http.delete(`/grocery/${id}`);
+};
+
+const removeAll = () => {
+    return http.delete(`/grocery`);
+};
+
+export default {
+    getAll,
+    get,
+    getByName,
+    getNeeded,
+    getPurchased,
+    add,
+    edit,
+    remove,
+    removeAll
+};
