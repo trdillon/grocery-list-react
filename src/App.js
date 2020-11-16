67ea32fe567f5bb528077c0561c9a5dd9b46a3e1
Tim Dillon <tim@itsdits.com>
2020-11-17 07:21:53 +0900

    Setup bootstrap, router and axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <div className="App"/>
+    );
+  }
 }
 
 export default App;
