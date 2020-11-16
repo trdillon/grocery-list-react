ae75362d1b9a13acf5c610de049004c994654971
Tim Dillon <tim@itsdits.com>
2020-11-17 08:32:55 +0900

    Created AddGrocery component

---
+++ src/component/AddGrocery.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import GroceryService from "../service/GroceryService";
+
+const AddGrocery = () => {
+    const initialGroceryState = {
+        id: null,
+        name: "",
+        quantity: null,
+        notes: "",
+        purchased: false
+    };
+
+    const [grocery, setGrocery] = useState(initialGroceryState);
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleInputChange = event => {
+        const { name, value } = event.target;
+        setGrocery({...grocery, [name]: value });
+    };
+
+    const saveGrocery = () => {
+        let data = {
+            name: grocery.name,
+            quantity: grocery.quantity,
+            notes: grocery.notes
+        };
+
+        GroceryService.add(data)
+            .then(response => {
+                setGrocery({
+                    id: response.data.id,
+                    name: response.data.name,
+                    quantity: response.data.quantity,
+                    purchased: response.data.purchased
+                });
+                setSubmitted(true);
+                console.log(response.data);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    };
+
+    const newGrocery = () => {
+        setGrocery(initialGroceryState);
+        setSubmitted(false);
+    };
+
+    return (
+        <div className="submit-form">
+            {submitted ? (
+                <div>
+                    <h4>Grocery item has been added successfully.</h4>
+                    <button className="btn btn-success" onClick={newGrocery}>
+                        Add
+                    </button>
+                </div>
+            ) : (
+                <div>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="name"
+                            required
+                            value={grocery.name}
+                            onChange={handleInputChange}
+                            name="name"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="quantity">Quantity</label>
+                        <input
+                            type="number"
+                            className="form-control"
+                            id="quantity"
+                            required
+                            value={grocery.quantity}
+                            onChange={handleInputChange}
+                            name="quantity"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="name">Notes</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="notes"
+                            value={grocery.notes}
+                            onChange={handleInputChange}
+                            name="notes"
+                        />
+                    </div>
+
+                    <button onClick={saveGrocery} className="btn btn-success">
+                        Submit
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default AddGrocery;
