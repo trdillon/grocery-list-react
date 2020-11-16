dfb9cc02e4ed0784864bcee5632c36b6f0dd715e
Tim Dillon <tim@itsdits.com>
2020-11-17 07:47:20 +0900

    Added navbar with Switch to handle Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-        <div className="App"/>
-    );
-  }
+function App() {
+  return(
+      <div className="app">
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <a href="https://itsdits.com" className="navbar-brand">
+                itsdits
+            </a>
+            <div className="navbar-nav mr-auto">
+                <li className="nav-item">
+                    <Link to={"/grocery"} className="nav-link">
+                        Grocery List
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to={"/add"} className="nav-link">
+                        Add
+                    </Link>
+                </li>
+            </div>
+        </nav>
+
+        <div className="container mt-3">
+            <Switch>
+                <Route exact path={["/", "/grocery"]} component={GroceryList} />
+                <Route exact path="/add" component={AddGrocery} />
+                <Route path="/grocery/:id" component={Grocery} />
+            </Switch>
+        </div>
+      </div>
+  );
 }
 
 export default App;
