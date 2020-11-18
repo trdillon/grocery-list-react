c19c633c2eb392630d229d4c277e80794ef5eb5e
Tim Dillon <tim@itsdits.com>
2020-11-18 09:47:08 +0900

    Refactored navbar into separate Header component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import AddGrocery from "./component/AddGrocery";
-import Grocery from "./component/Grocery";
-import GroceryList from "./component/GroceryList";
+import Header from "./component/Header";
 
 function App() {
   return(
       <div className="app">
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-            <a href="https://itsdits.com" className="navbar-brand">
-                itsdits
-            </a>
-            <div className="navbar-nav mr-auto">
-                <li className="nav-item">
-                    <Link to={"/grocery"} className="nav-link">
-                        Grocery List
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to={"/add"} className="nav-link">
-                        Add
-                    </Link>
-                </li>
-            </div>
-        </nav>
-
-        <div className="container mt-3">
-            <Switch>
-                <Route exact path={["/", "/grocery"]} component={GroceryList} />
-                <Route exact path="/add" component={AddGrocery} />
-                <Route path="/grocery/:id" component={Grocery} />
-            </Switch>
-        </div>
+        <Header />
       </div>
   );
 }
---
+++ src/component/Header.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {Link, Route, Switch} from "react-router-dom";
+import GroceryList from "./GroceryList";
+import AddGrocery from "./AddGrocery";
+import Grocery from "./Grocery";
+
+class Header extends Component {
+
+    render() {
+        return(
+            <header id="nav">
+                <nav className="navbar navbar-expand navbar-dark bg-dark">
+                    <a href="https://itsdits.com" className="navbar-brand">
+                        itsdits
+                    </a>
+                    <div className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link to={"/grocery"} className="nav-link">
+                                Grocery List
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to={"/add"} className="nav-link">
+                                Add
+                            </Link>
+                        </li>
+                    </div>
+                </nav>
+
+                <div className="container mt-3">
+                    <Switch>
+                        <Route exact path={["/", "/grocery"]} component={GroceryList} />
+                        <Route exact path="/add" component={AddGrocery} />
+                        <Route path="/grocery/:id" component={Grocery} />
+                    </Switch>
+                </div>
+            </header>
+        );
+    }
+}
+
+export default Header;
