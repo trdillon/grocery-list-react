abf35f898a28c59ec5dc8752fb4699a8a1c9dbe3
Tim Dillon <tim@itsdits.com>
2020-11-27 12:49:49 +0900

    Replaced React-GA with React-GTM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 import './custom.scss';
+import TagManager from 'react-gtm-module'
 
 import Header from "./component/Header";
+
+const tagManagerArgs = {
+    gtmId: 'GTM-xxxxxx'
+};
+
+TagManager.initialize(tagManagerArgs)
 
 function App() {
   return(
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ReactGA from 'react-ga';
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-ReactGA.initialize('UA-tracking-id');
-ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(
     <BrowserRouter>
         <App />
