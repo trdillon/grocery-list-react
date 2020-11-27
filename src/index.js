c6debeff538710644d6f1e9ac02e9316fff8024e
Tim Dillon <tim@itsdits.com>
2020-11-27 12:39:33 +0900

    Added Google Analytics, updated version number

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactGA from 'react-ga';
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
+ReactGA.initialize('UA-tracking-id');
+ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(
     <BrowserRouter>
         <App />
